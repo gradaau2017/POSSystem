ec11914ffc096cab173a31d8d758d276df19cfa3
Nathnael Getahun <contact.nathnael@gmail.com>
2017-12-23 09:20:51 +0300

    Added helper librarys and views

---
+++ MoencoPOS/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using NationalCriminalsDatabase.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ASPNetIdentity;
+
+namespace NationalCriminalsDatabase.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/Register
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                UserStore<ApplicationUser> Store = new UserStore<ApplicationUser>(new ApplicationDbContext());
+                ApplicationUserManager userManager = new ApplicationUserManager(Store);
+
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+    }
+}
