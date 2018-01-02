b7ec352ddb472cb68e20222b162546585d6312fd
gradaau2017 <33627318+gradaau2017@users.noreply.github.com>
2018-01-02 04:58:35 -0800

    Add files via upload

---
+++ MoencoPOS.Models/ProductTransferLineItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoencoPOS.Models
+{
+    public class ProductTransferLineItem
+    {
+        public int ProductTransferLineItemId { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+
+        public int ProductTransferId { get; set; }
+        public ProductTransfer ProductTransfer { get; set; }
+    }
+}
